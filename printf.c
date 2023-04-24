6494c56d7530e88851fe34293bc7dc481e133445
Yousef <yousefeldaly23@gmail.com>
2023-04-24 14:33:59 +0000

    0. I'm not going anywhere. You can print that wherever you want to. I'm here and I'm a Spur for life

---
+++ printf.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_c - handle c case after % in _printf
+ * @ap: argument pointer
+ */
+void print_c(va_list ap)
+{
+	 char c = va_arg(ap, int);
+
+	 putchar(c);
+}
+
+/**
+ * print_s - handle s case after % in _printf
+ * @ap: argument pointer
+ * Return: the length of string
+ */
+
+int print_s(va_list ap)
+{
+	int counter = 0;
+	char *str = va_arg(ap, char *);
+
+	for ( ; *str; str++, counter++)
+		putchar(*str);
+	return (counter);
+}
+
+/**
+ * _printf - prints based on format
+ * @format: identifier to look for
+ * Return: length of string
+ */
+
+
+int _printf(const char *format, ...)
+{
+	va_list ap;
+	int counter = 0;
+
+	if (format == NULL)
+	{
+		va_end(ap);
+		return (-1);
+	}
+	va_start(ap, format);
+
+	for ( ; *format; format++)
+	{
+		if (*format != '%')
+		{
+			putchar(*format);
+			counter += 1;
+		}
+		else
+		{
+			format++;
+			if (*format == 'c')
+			{
+				print_c(ap);
+				counter += 1;
+			}
+				counter += print_s(ap);
+				counter += print_s(ap);
+			}
+			else if (*format == '%')
+			{
+				putchar('%');
+			}
+		}
+	}
+
+	va_end(ap);
+	return (0);
+}
